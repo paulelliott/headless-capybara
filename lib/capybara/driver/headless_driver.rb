0f0cc56ed0d9db3dddaaa2119c09553395cd534f
Paul Elliott <paul@hashrocket.com>
2010-04-23 23:50:12 -0400

    Implement driver specs

--- lib/capybara/driver/headless_driver.rb
+++ lib/capybara/driver/headless_driver.rb
@@ -1,18 +1,74 @@
+require 'harmony'
+require 'nokogiri'
+
 class Capybara::Driver::Headless < Capybara::Driver::Base
 
   class Node < Capybara::Node
 
+    def text
+      node.text
+    end
+
+    def [](name)
+      node[name.to_s]
+    end
+
+    def set(value)
+      node['value'] = value
+    end
+
+    def tag_name
+      node.node_name
+    end
+
+    def visible?
+      node.xpath("./ancestor-or-self::*[contains(@style, 'display:none') or contains(@style, 'display: none')]").size == 0
+    end
+
+  private
+
+    def all_unfiltered(locator)
+      driver.find(node.xpath(locator).map { |n| n.path }.join(' | '))
+    end
+
   end
 
-  attr_accessor :page
-  class << self
-    alias fetch new
+  attr_reader :app, :rack_server, :page
+
+  def initialize(app)
+    @app = app
+    @rack_server = Capybara::Server.new(@app)
+    @rack_server.boot if Capybara.run_server
   end
 
-  def initialize(path)
-    @page = h_visit(path)
+  def visit(path)
+    @html = @body = nil
+    @page = Harmony::Page.fetch(url(path))
   end
 
+  def body
+    @body ||= page.to_html
+  end
+
+  def html
+    @html ||= Nokogiri::HTML(body)
+  end
+
+  def current_url
+    page.window.location
+  end
+
+  def find(selector)
+    html.xpath(selector).map { |node| Node.new(self, node) }
+  end
+
+private
+
+  def url(path)
+    rack_server.url(path)
+  end
+
+  ###OLD IMPLEMENTATION
   def click(selector)
     page.xw("$('#{selector}').click();")
   end
@@ -21,7 +77,7 @@ class Capybara::Driver::Headless < Capybara::Driver::Base
     page.x("$('#{selector}').text();")
   end
 
-private
+  private
 
   def ajax_setup(page)
     page.x %<
@@ -31,11 +87,6 @@ private
         }
       });
     >
-  end
-
-  def h_visit(path)
-    Harmony::Page.fetch("http://localhost:3001#{path}").
-      tap { |page| ajax_setup(page) }
   end
 
 end
