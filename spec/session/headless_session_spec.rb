d76171ac4df92a0be3c2f4714b6c40aeda28a45f
Paul Elliott <paul@hashrocket.com>
2010-04-24 09:18:17 -0400

    Fixed describe and added context to session spec

--- spec/session/headless_session_spec.rb
+++ spec/session/headless_session_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 
-describe Capybara::Driver::Headless do
-  before do
-    @session = Capybara::Session.new(:headless, TestApp)
-  end
-
-  describe '#driver' do
-    it "should be a headless driver" do
-      @session.driver.should be_an_instance_of(Capybara::Driver::Headless)
+describe Capybara::Session do
+  context 'with a headless driver' do
+    before do
+      @session = Capybara::Session.new(:headless, TestApp)
     end
-  end
 
-  describe '#mode' do
-    it "should remember the mode" do
-      @session.mode.should == :headless
+    describe '#driver' do
+      it "should be a headless driver" do
+        @session.driver.should be_an_instance_of(Capybara::Driver::Headless)
+      end
     end
+
+    describe '#mode' do
+      it "should remember the mode" do
+        @session.mode.should == :headless
+      end
+    end
+
+    it_should_behave_like "session"
+    it_should_behave_like "session with javascript support"
+    it_should_behave_like "session with headers support"
   end
-
-  it_should_behave_like "session"
-  it_should_behave_like "session with javascript support"
-  it_should_behave_like "session with headers support"
-
 end
