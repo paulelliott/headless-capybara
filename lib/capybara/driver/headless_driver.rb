471d730038ea1d27319351c421a75d9edd03a441
Paul Elliott <paul@hashrocket.com>
2010-04-27 12:51:31 -0400

    Replace eval'd js implementation with envjs direct integration

--- lib/capybara/driver/headless_driver.rb
+++ lib/capybara/driver/headless_driver.rb
@@ -6,44 +6,37 @@ class Capybara::Driver::Headless < Capybara::Driver::Base
   class Node < Capybara::Node
 
     def text
-      driver.evaluate_script(%<CHD.queried_objects[#{node}].innerText>)
+      node.innerText
     end
 
     def [](name)
       name = name.to_s
       name = 'className' if name == 'class'
-      driver.evaluate_script(%<CHD.queried_objects[#{node}].#{name}>)
+      node[name]
     end
 
     def set(value)
-      driver.evaluate_script(%<CHD.queried_objects[#{node}].value = "#{value}">)
+      node.value = value
     end
 
     def tag_name
-      driver.evaluate_script(%<CHD.queried_objects[#{node}].tagName>).downcase
+      node.tagName.downcase
     end
 
     def visible?
-      driver.evaluate_script(%<
-        var node = $HC(CHD.queried_objects[#{node}]);
-        node.is(':visible') && node.parents(':hidden').length == 0
-      >)
+      all_unfiltered("./ancestor-or-self::*[contains(@style, 'display:none') or contains(@style, 'display: none')]").empty?
     end
 
     def drag_to(element)
-      driver.evaluate_script(%<
-        CHD.queried_objects[#{node}].onmousedown();
-        CHD.queried_objects[#{element.node}].onmousemove();
-        CHD.queried_objects[#{element.node}].onmouseup();
-      >)
+      node.onmousedown
+      element.node.onmousemove
+      element.node.onmouseup
     end
 
   private
 
     def all_unfiltered(locator)
-      driver.evaluate_script(%<
-        CHD.find_by_xpath('#{locator}', CHD.queried_objects[#{node}])
-      >).split(',').map{ |key| Node.new(self, key) }
+      driver.find(locator, node)
     end
 
   end
@@ -59,26 +52,6 @@ class Capybara::Driver::Headless < Capybara::Driver::Base
 
   def visit(path)
     window.location = rack_url(path)
-    evaluate_script(%<
-      var CHD = {
-        object_count: 0,
-        queried_objects: {},
-        find_by_xpath: function(selector, context) {
-          if (context == null) { context = document; }
-          var response = [];
-          var q = document.evaluate(selector, context, null, XPathResult.ANY_TYPE, null)
-          var obj = q.iterateNext();
-          while (obj) {
-            response.push(CHD.object_count);
-            CHD.queried_objects[CHD.object_count] = obj;
-            CHD.object_count += 1;
-            obj = q.iterateNext();
-          }
-          return response.join(',');
-        }
-      };
-    >)
-    evaluate_script(JQUERY)
   end
 
   def body
@@ -91,8 +64,13 @@ class Capybara::Driver::Headless < Capybara::Driver::Base
     window.location.to_s
   end
 
-  def find(selector)
-    evaluate_script(%<CHD.find_by_xpath('#{selector}')>).split(',').map{ |key| Node.new(self, key) }
+  def find(selector, context = window.document)
+    elements = window.document.evaluate(selector, context, nil, window['XPathResult'].ANY_TYPE, nil)
+    nodes = []
+    while element = elements.iterateNext do
+      nodes << Node.new(self, element)
+    end
+    nodes
   end
 
   def evaluate_script(script)
@@ -101,7 +79,6 @@ class Capybara::Driver::Headless < Capybara::Driver::Base
 
   private
 
-  JQUERY = File.open(File.expand_path(File.join('lib','capybara','jquery-1.4.2.min.js'))).read
   BASE_RUNTIME = Johnson::Runtime.new
   BASE_RUNTIME.extend(Envjs::Runtime)
 
